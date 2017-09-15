227408afc58cd2536a0e965a844601fce5e3403f
Kyle Schrade <krschrade@oakland.edu>
2017-09-14 22:52:48 -0400

    update to use vuex for state management

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div id="app" class="columns is-multiline">
+    <div class="column is-12">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <img src="./assets/logo.png" height="35">
+          </a>
+        </div>
+        <div v-show="loggedIn">
+          <hr class="navbar-divider">
+          <div class="navbar-item">
+            <p>Home</p>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="column is-12">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import store from './Vuex/states'
+import {mapState} from 'vuex'
+
 export default {
-  name: 'app'
+  name: 'app',
+computed: mapState({
+      loggedIn: state => state.loggedIn
+})
 }
 </script>
 
