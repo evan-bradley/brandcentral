299a353eaee404dedc6496a386c3e364b502a19c
Joseph Herkness <jpherkness@gmail.com>
2017-09-24 16:36:30 -0400

    Move the navbar into a separate component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,7 @@
 <template>
   <div id="app" class="columns is-multiline">
     <div class="column is-12">
-      <nav v-show="this.$store.state.loggedIn" class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <img src="./assets/brand_central_full.png" height="50">
-          </a>
-        </div>
-        <div class="navbar-end">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              <img src="https://secure.gravatar.com/avatar/9c789dff1d20cc49a6f0b0df8c6308db?s=46&d=identicon" class="profile-image"/>
-              <p>jpherkness</p>
-            </a>
-            <div class="navbar-dropdown">
-              <div class="navbar-item">
-                <b>Joseph Herkness</b>
-              </div>
-              <router-link :to="{ name: 'Profile' }" class="navbar-item">Profile</router-link>
-              <a class="navbar-item">Settings</a>
-              <a class="navbar-item">Help</a>
-              <hr class="navbar-divider">
-              <a class="navbar-item">Sign out</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <navbar></navbar>
     </div>
     <div class="column is-12">
       <router-view></router-view>
@@ -34,8 +10,12 @@
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'navbar': Navbar
+  }
 }
 </script>
 
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav v-show="this.$store.state.loggedIn" class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="../assets/brand_central_full.png" height="50">
+      </a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          <img v-bind:src="'https://secure.gravatar.com/avatar/' + hash(User.Email) + '?s=46&d=identicon'" class="profile-image" />
+          <p>{{ User.UserName }}</p>
+        </a>
+        <div class="navbar-dropdown">
+          <div class="navbar-item" style="display:block;">
+            <b>{{ User.FirstName }} {{ User.LastName }}</b>
+            <br> @{{ User.UserName }}
+          </div>
+          <hr class="navbar-divider">
+          <router-link :to="{ name: 'Profile' }" class="navbar-item">Profile</router-link>
+          <a class="navbar-item">Settings</a>
+          <a class="navbar-item">About</a>
+          <hr class="navbar-divider">
+          <a class="navbar-item" @click="signOut">Sign out</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+var Classes = require('../TypeScriptFolder/Compliled/Classes').Classes
+var md5 = require('md5');
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      User: this.$store.state.User
+    }
+  },
+  methods: {
+    signOut() {
+      this.$store.commit('logOut')
+      this.$router.push({
+        name: 'Login'
+      })
+    },
+    hash(str) {
+      return md5(str)
+    }
+  }
+}
+</script>
