64602fcb5a47d28cd37ddf83f41bebcbe70aeeab
Joseph Herkness <jpherkness@gmail.com>
2017-09-30 16:49:13 -0400

    Fix test workflow

--- client/src/TypeScriptFolder/Classes.ts
+++ client/src/TypeScriptFolder/Classes.ts
@@ -1,34 +1,34 @@
-export namespace Classes {
+namespace Classes {
   export class User {
-    public Id: string;
-    public FirstName: string;
-    public LastName: string;
-    public UserName: string;
-    public Email: string;
-    public Password: string;
+    public Id: string
+    public FirstName: string
+    public LastName: string
+    public UserName: string
+    public Email: string
+    public Password: string
 
     constructor(id: string = "", userName: string = "", email: string = "", password: string = "", firstName: string = "", lastName: string = "") {
-      this.Id = id;
-      this.FirstName = firstName;
-      this.LastName = lastName;
-      this.UserName = userName;
-      this.Email = email;
-      this.Password = password;
+      this.Id = id
+      this.FirstName = firstName
+      this.LastName = lastName
+      this.UserName = userName
+      this.Email = email
+      this.Password = password
     }
   }
   export class PasswordVerification {
-    public CurrentPassword: string;
-    public NewPassword: string;
-    public VerifyNewPassword: string;
+    public CurrentPassword: string
+    public NewPassword: string
+    public VerifyNewPassword: string
 
     constructor(currentPassword: string = "", newPassword: string = "", verifyNewPassword: string = "") {
-      this.CurrentPassword = currentPassword;
-      this.NewPassword = newPassword;
-      this.VerifyNewPassword = verifyNewPassword;
+      this.CurrentPassword = currentPassword
+      this.NewPassword = newPassword
+      this.VerifyNewPassword = verifyNewPassword
     }
 
     Verify (currentPassword: string): boolean {
-      return (this.CurrentPassword === currentPassword && this.NewPassword === this.VerifyNewPassword);
+      return (this.CurrentPassword === currentPassword && this.NewPassword === this.VerifyNewPassword)
     }
   }
 }
