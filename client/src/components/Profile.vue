2049ff5a4cae5f24bfb22bcae83c0825732b63e1
Kyle Schrade <krschrade@oakland.edu>
2017-09-30 17:37:30 -0400

    updating work from git lab

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,11 +1,19 @@
-<template>
-  <div class="column is-10">
-    <div class="column is-12">
-      <img v-bind:src="'https://secure.gravatar.com/avatar/' + hash(User.Email) + '?s=46&d=identicon'" class="profile-image" />
-      <h1 class="title">{{ User.FirstName }} {{ User.LastName }}</h1>
-      <h1 class="subtitle">@{{ User.UserName }}</h1>
+<template> 
+    <div class="column is-10 columns is-multiline">
+        <div class="column is-12 is-centered">
+            <img v-bind:src="'https://secure.gravatar.com/avatar/' + hash(User.Email) + '?s=46&d=identicon'" class="profile-image" />
+            <h1 class="title">{{ User.FirstName }} {{ User.LastName }}</h1>
+            <h1 class="subtitle">@{{ User.UserName }}</h1>
+        </div>
+        <div>
+            <p class="subtitle">
+                Items you have liked:
+            </p>
+        </div>  
+       <div class="column is-2" v-for="pic in pictures" :key="pic.id">
+           <img :src="pic">
+       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -14,19 +22,20 @@ var md5 = require('md5');
 import EditProfile from './EditProfile'
 
 export default {
-  name: 'Profile',
-  data() {
-    return {
-      User: this.$store.state.User
+    name: 'Profile',
+    data() {
+        return {
+            User: this.$store.state.User,
+            pictures: []
+        }
+    },
+    components: {
+        'EditProfile': EditProfile
+    },
+    methods: {
+        hash(str) {
+            return md5(str)
+        }
     }
-  },
-  components: {
-    'EditProfile': EditProfile
-  },
-  methods: {
-    hash(str) {
-      return md5(str)
-    }
-  }
 }
 </script>
