1c19154678406e7f280f54c30b48e75703ee2f0b
Kyle Schrade <krschrade@oakland.edu>
2017-11-01 22:44:26 -0400

    inital serge of work

---
+++ Mobile/Mobile/Mobile/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Mobile.Helpers;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnSaveActivated(object sender, EventArgs e)
+        {
+            HttpHelpers httpHelpers = new HttpHelpers();
+
+            HttpResponseMessage testing = httpHelpers.LogIn("username", "password");
+            
+
+
+        }
+    }
+}
