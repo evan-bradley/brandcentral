a3d7454994f608581cb3c9f462295ffe227b26d9
Brandon J <bwjackson@oakland.edu>
2017-11-18 15:46:30 -0500

    Updated prediction function to update CNN_RESULTS table

--- CNN Testing/predictProducts.py
+++ CNN Testing/predictProducts.py
@@ -2,83 +2,74 @@ import tensorflow as tf
 import numpy as np
 from sklearn.utils import shuffle
 import os,glob,cv2
+import time
 import sys,argparse
 import urllib.request
 from PIL import Image
 import MySQLdb
 
-def predict(channelID):
+def updatePredictions():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     image_path = 'product.jpg'
     filename = dir_path + '/' + image_path
+
+    image_size = 128
+    num_channels = 3
+
+    updateStmt = ""
+
     try:
-        # db = MySQLdb.connect(host="138.197.85.34", user="root", password="somethingeasy", db="BRAND_CENTRAL")
-        db = MySQLdb.connect(host="localhost", user="root", db="BRAND_CENTRAL")
+        db = MySQLdb.connect(host="138.197.85.34", user="root", password="somethingeasy", db="BRAND_CENTRAL")
+        # db = MySQLdb.connect(host="localhost", user="root", db="BRAND_CENTRAL")
         cursor = db.cursor()
     except MySQLdb.Error:
         print("Error in Connection")
 
-    sqlGetProducts = "SELECT p.PRODUCT_ID, p.PROD_PICT_URL FROM PRODUCT AS p JOIN PROD_TAG_ASSIGN AS pt ON p.PRODUCT_ID = pt.PRODUCT_ID JOIN CHANNEL_TAG_ASSIGN AS ct ON pt.TAG_ID = ct.TAG_ID WHERE ct.CHANNEL_ID = "+str(channelID)
-    cursor.execute(sqlGetProducts)
+    sqlGetClusterProducts = "SELECT PRODUCT_ID, PROD_PICT_URL FROM PRODUCT"
+    cursor.execute(sqlGetClusterProducts)
+    products = cursor.fetchall()
 
-    i = 0
-    products = []
-    while i < cursor.rowcount:
-        row = cursor.fetchone()
-        products.append(row)
-        i += 1
-    products = shuffle(products)
+    sqlGetClusters = "SELECT DISTINCT CLUSTER_ID FROM CNN_RESULTS"
+    cursor.execute(sqlGetClusters)
+    clusterCount = cursor.rowcount
 
-    for product in range(0, len(products)):
-        urllib.request.urlretrieve(products[product][1],'product.jpg')
-
-        image_size=128
-        num_channels=3
-        images = []
-        # Reading the image using OpenCV
-        image = cv2.imread(filename)
-        # Resizing the image to our desired size and preprocessing will be done exactly as done during training
-        image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
-        images.append(image)
-        images = np.array(images, dtype=np.uint8)
-        images = images.astype('float32')
-        images = np.multiply(images, 1.0/255.0)
-        #The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
-        x_batch = images.reshape(1, image_size,image_size,num_channels)
-
-        ## Let us restore the saved model
+    for cluster in range(1, clusterCount+1):
+        # Restore the saved model
         sess = tf.Session()
         # Step-1: Recreate the network graph. At this step only graph is created.
         saver = tf.train.import_meta_graph('like-dislike-model.meta')
         # Step-2: Now let's load the weights saved using the restore method.
         saver.restore(sess, tf.train.latest_checkpoint('./'))
 
-        # Accessing the default graph which we have restored
+        # Accessing the restored graph
         graph = tf.get_default_graph()
-
-        # Now, let's get hold of the op that we can be processed to get the output.
-        # In the original network y_pred is the tensor that is the prediction of the network
         y_pred = graph.get_tensor_by_name("y_pred:0")
 
-        ## Let's feed the images to the input placeholders
-        x= graph.get_tensor_by_name("x:0")
+        # Feed the images to the input placeholders
+        x = graph.get_tensor_by_name("x:0")
         y_true = graph.get_tensor_by_name("y_true:0")
         y_test_images = np.zeros((1, 2))
 
+        for product in range(1, len(products)+1):
+            urllib.request.urlretrieve(products[product][1], 'product.jpg')
 
-        ### Creating the feed_dict that is required to be fed to calculate y_pred
-        feed_dict_testing = {x: x_batch, y_true: y_test_images}
-        result=sess.run(y_pred, feed_dict=feed_dict_testing)
-        # result is of this format [probabiliy_of_rose probability_of_sunflower]
-        imagePrint = Image.open('product.jpg')
-        imagePrint.show()
-        input()
-        print("Like Probability: ",result[0][0],"\nDislike Probability: ",result[0][1])
+            # Reading the image using OpenCV
+            image = cv2.imread(filename)
+            # Resizing the image to our desired size and preprocessing will be done exactly as done during training
+            image = cv2.resize(image, (image_size, image_size), cv2.INTER_LINEAR)
+            image = np.array(image, dtype=np.uint8)
+            image = image.astype('float32')
+            image = np.multiply(image, 1.0 / 255.0)
+            # The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
+            x_batch = image.reshape(1, image_size, image_size, num_channels)
 
-        if result[0][0] > 0.5:
-            print(products[product][0])
-            return products[product][0]
-    return -1
+            # Creating the feed_dict that is required to be fed to calculate y_pred
+            feed_dict_testing = {x: x_batch, y_true: y_test_images}
+            result = sess.run(y_pred, feed_dict=feed_dict_testing)
 
-
-predict(1)
+            curTime = time.strftime('%Y-%m-%d %H:%M:%S')
+            strUpdate = "UPDATE CNN_RESULTS SET LIKE_PCT = "+str(result[0][0])+", LAST_UPDATE = '"+curTime+"' WHERE CLUSTER_ID = "+str(cluster)+" AND PRODUCT_ID = "+str(product)
+            print(strUpdate)
+            cursor.execute(strUpdate)
+            db.commit()
+updatePredictions()
