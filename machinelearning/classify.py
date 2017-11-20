3425ec6a90fe6e94757c59866e9f4188bdeb0088
Erik <eculberson12@gmail.com>
2017-11-20 13:48:05 -0500

    Add predictions and clean code

--- machinelearning/classify.py
+++ machinelearning/classify.py
@@ -73,58 +73,30 @@ def calculate_user_weight_vectors(vector_length):
             user_weights[item['uid']] = np.subtract(user_weights[item['uid']], product_vectors[item['pid']])
         return user_weights
 
+def calculate_all_weight_vectors(vector_length, weights):
+    # Compute the product vectors
+    product_vectors = calculate_product_vectors(vector_length)
+
+    dataset = []
+    for item in product_vectors:
+       dataset.append(item * weights)
+
+    return dataset
+
 def cluster_users(number_of_clusters):
     """
     Computes the cluster means for the specified number of clusters.
     """
     # Get the user weight vectors, because we want to cluster users
-    user_weights = calculate_user_weight_vectors(300)
+    user_weights = calculate_user_weight_vectors(200)
 
     # Perform clustering using K Means
     user_weights_array = np.array(list(user_weights.values()))
-    print(user_weights_array.shape)
     kmeans = KMeans(n_clusters=number_of_clusters).fit(user_weights_array)
     print("Number of Clusters:", number_of_clusters)
     print("Cluster Labels:", kmeans.labels_)
     print("Cluster Means:", kmeans.cluster_centers_)
-    return kmeans.labels_
-
-def data(vector_length):
-    product_vectors = calculate_product_vectors(vector_length)
-
-    with connection.cursor() as cursor:
-        # Get the products that the user likes
-        sql = "SELECT USER_ID as uid, PRODUCT_ID as pid FROM LIKES"
-        cursor.execute(sql)
-        like_results = cursor.fetchall()
-
-        # Get the products that the user dislikes
-        sql = "SELECT USER_ID as uid, PRODUCT_ID as pid FROM DISLIKES"
-        cursor.execute(sql)
-        dislike_result = cursor.fetchall()
-
-        # Construct the user wights by adding liked products and subtracting disliked products
-        user_weights = {}
-        for item in like_results:
-            if item['uid'] not in user_weights:
-                user_weights[item['uid']] = np.zeros(vector_length)
-            user_weights[item['uid']] = np.add(user_weights[item['uid']], product_vectors[item['pid']])
-
-        for item in dislike_result:
-            if item['uid'] not in user_weights:
-                user_weights[item['uid']] = np.zeros(vector_length)
-            user_weights[item['uid']] = np.subtract(user_weights[item['uid']], product_vectors[item['pid']])
-
-        print(len(user_weights))
-        # Multiply each product by the like/dislike and add it to the dataset
-        dataset = {'data':[], 'label':[]}
-        for item in like_results:
-            dataset['data'].append(user_weights[item['uid']] * product_vectors[item['pid']])
-            dataset['label'].append(1)
-        for item in dislike_result:
-            dataset['data'].append(user_weights[item['uid']] * product_vectors[item['pid']])
-            dataset['label'].append(0)
-        return dataset
+    return kmeans.labels_, kmeans.cluster_centers_
 
 def data_for_uid(vector_length, uid):
     product_vectors = calculate_product_vectors(vector_length)
@@ -155,7 +127,6 @@ def data_for_uid(vector_length, uid):
                 user_weights[item['uid']] = np.zeros(vector_length)
             user_weights[item['uid']] = np.subtract(user_weights[item['uid']], product_vectors[item['pid']])
 
-        print(len(user_weights))
         # Multiply each product by the like/dislike and add it to the dataset
         dataset = {'data':[], 'label':[]}
         for item in like_results:
@@ -166,93 +137,12 @@ def data_for_uid(vector_length, uid):
             dataset['label'].append(0)
         return dataset
 
-def data_train_test():
-    dataset = data(200)
-    X = dataset['data']
-    y = dataset['label']
-
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    # Fit only to the training data
-    scaler.fit(X_train)
-    # Now apply the transformations to the data:
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    return X, y, X_train, X_test, y_train, y_test
-
 def data_train_test_for_uids(uids):
     dataset = data_for_uid(200, uids)
     X = dataset['data']
     y = dataset['label']
 
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    # Fit only to the training data
-    scaler.fit(X_train)
-    # Now apply the transformations to the data:
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    return X, y, X_train, X_test, y_train, y_test
-
-def classify():
-    from matplotlib.colors import ListedColormap
-    from sklearn.model_selection import train_test_split
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.datasets import make_moons, make_circles, make_classification
-    from sklearn.neural_network import MLPClassifier
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.svm import SVC
-    from sklearn.gaussian_process import GaussianProcessClassifier
-    from sklearn.gaussian_process.kernels import RBF
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier
-    from sklearn.naive_bayes import GaussianNB
-    from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-    from sklearn.model_selection import cross_val_score, cross_val_predict
-
-    # Retrieve the training data and test data
-    X, y, X_train, X_test, y_train, y_test = data_train_test()
-
-    # Specify the classifiers we would like to test
-    classifiers = {
-        "Nearest Neighbors (1)": KNeighborsClassifier(1),
-        "Nearest Neighbors (3)": KNeighborsClassifier(3),
-        "Nearest Neighbors (5)": KNeighborsClassifier(5),
-        "Nearest Neighbors (7)": KNeighborsClassifier(7),
-        "Naive Bayes": GaussianNB(),
-        "Neural Net (30, 30, 30)": MLPClassifier(hidden_layer_sizes=(30,30,30), max_iter=1000),
-        "Neural Net NHL": MLPClassifier(max_iter=1000),
-        "Decision Tree": DecisionTreeClassifier(max_depth=5),
-        "Random Forest (Ensemble)": RandomForestClassifier(max_depth=30, n_estimators=100, max_features=1),
-        "Linear SVM": SVC(kernel="linear", C=0.025),
-        "RBF SVM": SVC(gamma=2, C=1),
-        "AdaBoost": AdaBoostClassifier(),
-        "Voting (Ensemble)": VotingClassifier(estimators=[
-            ('nn1', MLPClassifier(max_iter=1000)),
-            ('nn2', MLPClassifier(max_iter=1000)),
-            ('nn3', MLPClassifier(max_iter=1000))], voting='hard')
-    }
-
-    # Compute the accuracy of each classifier using cross validation
-    accuracies = {}
-    for name, clf in classifiers.items():
-        print('Training', name, '...')
-        scores = cross_val_score(clf, X, y, cv=5)
-        accuracies[name] = np.mean(scores)
-        
-    # Print a sorted list of accuracies to the console
-    i = 1
-    for key, value in sorted(accuracies.items(), key=operator.itemgetter(1), reverse=True):
-        print("%-*s) %-*s: %s" % (2, i, 25, key, value))
-        i += 1
+    return X, y
 
 def classify_for_uids(uids):
     from matplotlib.colors import ListedColormap
@@ -271,22 +161,25 @@ def classify_for_uids(uids):
     from sklearn.model_selection import cross_val_score, cross_val_predict
 
     # Retrieve the training data and test data
-    X, y, X_train, X_test, y_train, y_test = data_train_test_for_uids(uids)
+    X, y = data_train_test_for_uids(uids)
+
+    if len(X) == 0:
+        return 0
 
     # Specify the classifiers we would like to test
     classifiers = {
-        "Nearest Neighbors (1)": KNeighborsClassifier(1),
-        "Nearest Neighbors (3)": KNeighborsClassifier(3),
-        "Nearest Neighbors (5)": KNeighborsClassifier(5),
-        "Nearest Neighbors (7)": KNeighborsClassifier(7),
-        "Naive Bayes": GaussianNB(),
-        "Neural Net (30, 30, 30)": MLPClassifier(hidden_layer_sizes=(30,30,30), max_iter=1000),
-        "Neural Net NHL": MLPClassifier(max_iter=1000),
-        "Decision Tree": DecisionTreeClassifier(max_depth=5),
-        "Random Forest (Ensemble)": RandomForestClassifier(max_depth=30, n_estimators=100, max_features=1),
-        "Linear SVM": SVC(kernel="linear", C=0.025),
-        "RBF SVM": SVC(gamma=2, C=1),
-        "AdaBoost": AdaBoostClassifier(),
+        # "Nearest Neighbors (1)": KNeighborsClassifier(1),
+        # "Nearest Neighbors (3)": KNeighborsClassifier(3),
+        # "Nearest Neighbors (5)": KNeighborsClassifier(5),
+        # "Nearest Neighbors (7)": KNeighborsClassifier(7),
+        # "Naive Bayes": GaussianNB(),
+        # "Neural Net (30, 30, 30)": MLPClassifier(hidden_layer_sizes=(30,30,30), max_iter=1000),
+        # "Neural Net NHL": MLPClassifier(max_iter=1000),
+        # "Decision Tree": DecisionTreeClassifier(max_depth=5),
+        # "Random Forest (Ensemble)": RandomForestClassifier(max_depth=30, n_estimators=100, max_features=1),
+        # "Linear SVM": SVC(kernel="linear", C=0.025),
+        # "RBF SVM": SVC(gamma=2, C=1),
+        # "AdaBoost": AdaBoostClassifier(),
         "Voting (Ensemble)": VotingClassifier(estimators=[
             ('nn1', MLPClassifier(max_iter=1000)),
             ('nn2', MLPClassifier(max_iter=1000)),
@@ -305,7 +198,11 @@ def classify_for_uids(uids):
     for key, value in sorted(accuracies.items(), key=operator.itemgetter(1), reverse=True):
         print("%-*s) %-*s: %s" % (2, i, 25, key, value))
         i += 1
-        
+
+    return(VotingClassifier(estimators=[
+        ('nn1', MLPClassifier(max_iter=1000)),
+        ('nn2', MLPClassifier(max_iter=1000)),
+        ('nn3', MLPClassifier(max_iter=1000))], voting='hard').fit(X,y))
 
 def print_header(text):
     print("======================================================")
@@ -319,7 +216,7 @@ def rabbitmq_setup():
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     def callback(ch, method, properties, body):
-        update_db(1) 
+        update_db(5) 
         print(" [x] Received %r" % body)
 
     channel.basic_consume(callback, queue='train', no_ack=True)
@@ -327,22 +224,34 @@ def rabbitmq_setup():
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-def update_db(cluster_id):
-    print(cluster_id)
-    clustered_users = cluster_users(5)
-    print(clustered_users)
-    classify_for_uids([2])
+def update_db(cluster_size):
+    print("Running update db")
+    print("Clustering users")
+    clustered_users, centers = cluster_users(cluster_size)
+    for cluster_id in range(cluster_size):
+        print("Grouping users")
+        users = []
+        for inx in range(len(clustered_users)):
+            if clustered_users[inx] == cluster_id:
+                users.append(inx)
+        print("Training users")
+        classifier = classify_for_uids(users)
+        if classifier == 0:
+            continue
+        print("Predicting")
+        data = calculate_all_weight_vectors(200, centers[cluster_id])
+        pred = classifier.predict(data)
+        from collections import Counter
+        print(Counter(pred).keys())
+        print(Counter(pred).values())
+        #print(classifier.predict())
     
 
 def main():
     try:
-        print_header("Clustering")
-        cluster_users(5)
-
-        print_header("Classification")
-        classify()
-
+        update_db(5)
         rabbitmq_setup()
+
     finally:
         connection.close()
 
