7f4790cb83dee6c1660bdd4ea9a90d846d8b5c73
Joseph Herkness <jpherkness@gmail.com>
2017-12-01 14:34:44 -0500

    Add buttons to user row and channel row

--- client/src/components/ChannelRow.vue
+++ client/src/components/ChannelRow.vue
@@ -1,13 +1,29 @@
 <template>
-  <li class="channel-row" v-bind:class="{ 'is-active': channel.id == $route.params.channelId }">
-    <router-link v-on:click.native="select()" class="item-link channel-row-link" :to="{ name: 'channel', params:{ channelId: channel.id } }">
-      <span class="tag">
-        <!-- <i class="fa fa-tag" aria-hidden="true"></i> -->
-        <i class="material-icons md-16">local_offer</i>
-      </span>
-      <span class="channel-name">{{ channel.name }}</span>
-    </router-link>
-  </li>
+  <router-link v-on:click.native="select()" 
+    class="channel-row hover-container item-link channel-row-link" 
+    v-bind:class="{ 'is-active': channel.id == $route.params.channelId }" 
+    :to="{ name: 'channel', params:{ channelId: channel.id } }">
+    <span class="tag">
+      <i class="material-icons md-16">local_offer</i>
+    </span>
+    <span class="channel-name">{{ channel.name }}</span>
+    <div class="hover-content right">
+      <p v-if="subscribed()" class="control display-on-hover">
+        <a class="button like-button" v-on:click.stop.prevent="unsubscribe">
+          <span class="icon is-small">
+            <i class="material-icons md-16">close</i>
+          </span>
+        </a>
+      </p>
+      <p v-else class="control">
+        <a class="button like-button" v-on:click.stop.prevent="subscribe">
+          <span class="icon is-small">
+            <i class="material-icons md-16">add</i>
+          </span>
+        </a>
+      </p>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -22,6 +38,35 @@
     methods: {
       select () {
         this.$parent.$emit('selectedChannel')
+      },
+      subscribed () {
+        // Returns a boolean indicating if the user is subscribed to the current channel
+        var channelId = this.channel.id
+        var channels = this.$store.state.channels
+        var subbed = channels.some(function (element) {
+          return element.id === channelId
+        })
+        return subbed
+      },
+      subscribe () {
+        this.$http.post(`/api/channels/subscribe/${this.channel.id}`)
+        .then(response => {
+          if (response.data.success) {
+            this.$store.commit('addChannel', this.channel)
+          }
+        }, response => {
+          console.log('Failed to subscribe')
+        })
+      },
+      unsubscribe () {
+        this.$http.post(`/api/channels/unsubscribe/${this.channel.id}`)
+        .then(response => {
+          if (response.data.success) {
+            this.$store.commit('removeChannel', this.channel.id)
+          }
+        }, response => {
+          console.log('Failed to unsubscribe')
+        })
       }
     }
   }
--- client/src/components/UserRow.vue
+++ client/src/components/UserRow.vue
@@ -1,13 +1,31 @@
 <template>
-  <li class="user-row" v-bind:class="{ 'is-active': user.id == $route.params.userId }">
-    <router-link v-on:click.native="select()" class="item-link user-row-link" :to="{ name: 'profile', params:{ userId: user.id } }">
-      <img class="profile-image" v-bind:src="`https://secure.gravatar.com/avatar/${user.emailHash}?s=32&d=identicon`" />
-      <div class="information-container">
-        <span class="name" v-if="this.shouldDisplayName">{{ user.firstName }} {{ user.lastName}}</span>
-        <span class="username">{{ user.username }}</span>
-      </div>
-    </router-link>
-  </li>
+  <router-link class="user-row hover-container item-link user-row-link" 
+    v-bind:class="{ 'is-active': user.id == $route.params.userId }" 
+    v-on:click.native="select()"
+    :to="{ name: 'profile', params:{ userId: user.id } }">
+    <img class="profile-image"
+      v-bind:src="`https://secure.gravatar.com/avatar/${user.emailHash}?s=32&d=identicon`" />
+    <div class="information-container">
+      <span class="name" v-if="this.shouldDisplayName">{{ user.firstName }} {{ user.lastName}}</span>
+      <span class="username">{{ user.username }}</span>
+    </div>
+    <div class="hover-content right">
+      <p v-if="following()" class="control display-on-hover">
+        <a class="button like-button" v-on:click.stop.prevent="unfollow">
+          <span class="icon is-small">
+            <i class="material-icons md-16">close</i>
+          </span>
+        </a>
+      </p>
+      <p v-else class="control">
+        <a class="button like-button" v-on:click.stop.prevent="follow">
+          <span class="icon is-small">
+            <i class="material-icons md-16">add</i>
+          </span>
+        </a>
+      </p>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -27,6 +45,38 @@
     methods: {
       select () {
         this.$parent.$emit('selectedUser')
+      },
+      following () {
+        var userId = this.user.id
+        var followedUsers = this.$store.state.followedUsers
+        var following = followedUsers.some(function (element) {
+          return element.id === userId
+        })
+        return following
+      },
+      follow () {
+        this.$http.post(`/api/user/follow/${this.user.id}`)
+        .then(response => {
+          if (response.data.success) {
+            var user = {
+              id: this.user.id,
+              username: this.user.username
+            }
+            this.$store.commit('addFollowedUser', user)
+          }
+        }, response => {
+          console.log('Failed to follow')
+        })
+      },
+      unfollow () {
+        this.$http.post(`/api/user/unfollow/${this.user.id}`)
+        .then(response => {
+          if (response.data.success) {
+            this.$store.commit('removeFollowedUser', this.user.id)
+          }
+        }, response => {
+          console.log('Failed to unfollow')
+        })
       }
     }
   }
