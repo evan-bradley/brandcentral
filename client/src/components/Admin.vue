1a79df520d289e859b6878b537601e2ff8583332
Matthew DeMott <demottmatthew@gmail.com>
2017-12-10 12:52:45 -0500

    fixed a lot of bugs in the admin.vue file, however there is still a problem with displaying the tagid and channelid

--- client/src/components/Admin.vue
+++ client/src/components/Admin.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <!-- 
+    <!--
       After logging in replace the url with /admin it should bring you to this vue file.
       the first section pops up if there is no current channel. and allows you to select a channel
-      the bottom half displays all the products and gives you a button to press to remove a products 
+      the bottom half displays all the products and gives you a button to press to remove a products
       tag. also added some logging to display post request responses.
     -->
       <div class="columns is-multiline" v-show="currentChannel === null">
@@ -13,21 +13,34 @@
       </div>
       <div v-if="currentChannel">
           <div class="columns is-multiline">
+            <div class="column is-12"> {{ currentChannel.name }} </div>
               <div class="column is-12">
-                  <button class="button is-primary" @click="currentChannel = null">Back to channel select</button>
+                <button class="button is-primary" @click="currentChannel = null">Back to channel select</button>
+              </div>
+              <div class="column is-12">
+                <button class="button is-primary" @click="loadLikedProducts(1)">Load Products</button>
               </div>
               <div class="column is-one-quarter is-half-tablet is-12-mobile" v-for="product in likedProducts" :key="product.id">
-                  <div class="columns is-multiline">
-                      <div class="column is-12">
-                          <h3 class="title is-4"> {{ product.name }} </h3>
-                      </div>
-                      <div class="column is-12">
-                          <img :src="product.pictureUrl" alt="Placeholder image" class="column is-12">
-                      </div>
-                      <div class="column is-12">
-                          <button class="button is-primary" @click="RemoveFromChannel(product)">Remove from channel</button>
-                      </div>
+                <div class="box">
+                  <div>
+                    <div class="image-container image is-square">
+                      <img :src="product.pictureUrl" alt="Placeholder image">
+                    </div>
+
+                    <div class="content has-text-left">
+                      {{ product.name.substring(0, 80) + '...' }}
+                    </div>
                   </div>
+                  <div class="content has-text-left">
+                    Channel ID: {{ product.channelid }}
+                  </div>
+                  <div class="content has-text-left">
+                    Tag ID: {{ product.tagid }}
+                  </div>
+                <div class="column is-12">
+                  <button class="button is-primary" @click="RemoveFromChannel(product)">Remove from channel</button>
+                </div>
+                </div>
               </div>
           </div>
           <nav class="pagination" v-show="totalProducts > numberPerPage" role="navigation" aria-label="pagination">
@@ -39,79 +52,91 @@
 </template>
 
 <script>
-export default {
-  name: 'Admin',
-  data () {
-    return {
-      currentChannel: null,
-      userChannels: [],
-      likedProducts: [],
-      currentPage: 1,
-      numberPerPage: 20,
-      totalProducts: 0,
-      searchText: ''
-    }
-  },
-  watch: {
-    currentChannel (channel) {
-      if (channel === null) {
-        this.loadLikedProducts(channel)
+  import ProductItem from './ProductItem.vue'
+  var Classes = require('../TypeScriptFolder/Compiled/Classes').Classes
+  export default {
+    name: 'Admin',
+    data () {
+      return {
+        currentChannel: null,
+        userChannels: [],
+        likedProducts: [],
+        currentPage: 1,
+        numberPerPage: 20,
+        totalProducts: 0,
+        searchText: ''
       }
-    }
-  },
-  created () {
-    this.loadChannels()
-  },
-  methods: {
-    loadChannels () {
-      this.$http(`/api/user/${this.$store.state.User.Id}/channels`).then(
-        response => {
-          console.log(response)
-          if (response.data.success) {
-            response.body.products.forEach(function (el) {
-              this.userChannels.push(el)
-            }, this)
-          }
+    },
+    watch: {
+      currentChannel (channel) {
+        if (channel === null) {
+          this.loadLikedProducts(1)
         }
-      )
-    },
-    loadLikedProducts (page) {
-      // if (page === this.currentPage) return
-      var userId = this.$store.state.User.Id
-      this.currentPage = page
-      var newlikedProducts = []
-      var url = `/api/channel/products/${this.currentChannel.Id}?productsPer=${this.numberPerPage}&page=${page}`
-
-      this.$http.get(url).then(
-        response => {
-          console.log(response)
-          if (response.data.success) {
-            response.body.products.forEach(function (el) {
-              newlikedProducts.push(el)
-            }, this)
-            this.likedProducts = newlikedProducts
-            this.totalProducts = response.body.total
-          }
-        },
-        response => {
-          console.log(response)
-          console.log(`Failed to load products. userId=${userId}, page=${page}, numPerPage=${this.numPerPage}`)
-        }
-      )
-    },
-    previousPage () {
-      this.loadLikedProducts(this.currentPage - 1)
-    },
-    nextPage () {
-      this.loadLikedProducts(this.currentPage + 1)
-    },
-    RemoveFromChannel (product) {
-      var payload = {
-        tagid: product.tagid
       }
-      this.$http.post(`/api/tag/delete/${product.id}`, payload)
-      this.loadLikedProducts(this.currentChannel)
+    },
+    components: {
+      'ProductItem': ProductItem
+    },
+    created () {
+      this.loadChannels()
+    },
+    methods: {
+      loadChannels () {
+        this.$http.get(`/api/user/${this.$store.state.User.Id}/channels`)
+          .then(response => {
+            console.log(response.data.channels)
+            if (response.data.success) {
+              response.data.channels.forEach(function (el) {
+                this.userChannels.push(el)
+              }, this)
+            }
+          },
+          response => {
+            console.log(response.data.channels)
+            console.log('Failed')
+          }
+        )
+      },
+      loadLikedProducts (page) {
+        // if (page === this.currentPage) return
+        var userId = this.$store.state.User.Id
+        this.currentPage = page
+        var newlikedProducts = []
+        var url = `/api/channel/products/${this.currentChannel.id}?productsPer=${this.numberPerPage}&page=${page}`
+        this.$http.get(url)
+          .then(response => {
+            console.log('test')
+            console.log(response.body.products)
+            console.log('test')
+            if (response.data.success) {
+              response.body.products.forEach(function (el) {
+                var product = new Classes.Product(el.id, el.name, el.description, el.pictureUrl, el.productUrl, el.channelid, el.tagid)
+                console.log(product.channelid)
+                newlikedProducts.push(product)
+              }, this)
+              this.likedProducts = newlikedProducts
+              this.totalProducts = response.body.total
+            }
+          },
+          response => {
+            console.log(response)
+            console.log(`Failed to load products. userId=${userId}, page=${page}, numPerPage=${this.numPerPage}`)
+          }
+        )
+      },
+      previousPage () {
+        this.loadLikedProducts(this.currentPage - 1)
+      },
+      nextPage () {
+        this.loadLikedProducts(this.currentPage + 1)
+      },
+      RemoveFromChannel (product) {
+        var body = {
+          tagid: product.tagid
+        }
+        this.$http.post(`/api/tag/delete/${product.id}`, body)
+        this.loadLikedProducts(this.currentPage)
+      }
     }
   }
-}
 </script>
